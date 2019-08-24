f5df4b2dc24b856caf5f15f8e7da368d8ad540ee
davadeola <adeoladavid15@gmail.com>
2019-08-24 16:19:47 +0300

    Added a createScream firebase function

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,10 @@ admin.initializeApp();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
+
+
+//defining my functions
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Fire!");
 });
@@ -25,4 +29,28 @@ exports.getScreams = functions.https.onRequest((req, res)=>{
   .catch(err=>{
     console.error(err);
   })
+});
+
+
+exports.createScream = functions.https.onRequest((req, res)=>{
+  if(req.method !== POST){
+    return res.status(400).json({error: `Method not allowed`});
+  }
+
+  const newScream={
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+  };
+
+  admin.firestore()
+  .collection('screams')
+  .add(newScream)
+  .then((doc)=>{
+    res.json({message: `document ${doc.id} created successfully`});
+  })
+  .catch((err)=>{
+    res.status(500).json({error: `something went wrong`});
+    console.error(err);
+  });
 });
