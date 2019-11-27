206f5ec54a9ee3c3b94a2c43ad5782161d6c22dd
davadeola <adeoladavid15@gmail.com>
2019-11-27 16:30:07 +0300

    Refactored routes and utilities into separate files

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyCt-CSHlWb8WhNkGvNwvWhwz7bU9j0RGO8",
+  authDomain: "socialapp-f352e.firebaseapp.com",
+  databaseURL: "https://socialapp-f352e.firebaseio.com",
+  projectId: "socialapp-f352e",
+  storageBucket: "socialapp-f352e.appspot.com",
+  messagingSenderId: "473185452166",
+  appId: "1:473185452166:web:ee515cde4f35c328"
+};
